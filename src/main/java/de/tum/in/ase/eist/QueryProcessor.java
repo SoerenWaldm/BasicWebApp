b4b1ce0cbe86f95ca77cf65a6002dd7bc1fc6934
DESKTOP-AUMGFIC\soere <ge64qix@mytum.de>
2022-07-13 11:27:17 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,6 +13,19 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("Max")) {
            return "Max";
+        } else if(query.contains("plus")) {
+            String[] params = query.split(" ");
+            int sus = 0;
+            for(String s : params) {
+                try {
+                    int n = Integer.parseInt(s);
+                    sum += n
+                } catch (NumberFormatException e) {
+                    //comment
+                }
+            }
+            return String.valueOf(sum);
+        }
         } else { // TODO extend the programm here
             return "";
         }
